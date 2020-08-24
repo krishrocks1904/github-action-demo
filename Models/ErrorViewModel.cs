4b4b82d2b6ab7d65583208c5ef3709fa2bf24e5e
rakesh.suryawanshi <mrrsuryawanshi@gmail.com>
2020-08-24 15:22:05 +0100

    adding new dotnet core app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace github_action_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
